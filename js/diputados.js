5782d88c9bcb81a74584fd77383767c877f4ffc2
rajras <rajras@zennovia.com>
2020-08-25 15:40:25 -0300

    reclamo diputados.

---
+++ js/diputados.js
@@ -0,0 +1,451 @@
+var jsonDiputados = [
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mcampagnoli.jpg",
+        "Diputado": "Campagnoli, Marcela",
+        "Bloque": "COALICION CIVICA",
+        "Correo": "mcampagnoli@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/jcampos.jpg",
+        "Diputado": "Campos, Javier",
+        "Bloque": "COALICION CIVICA",
+        "Correo": "jcampos@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/hflores.jpg",
+        "Diputado": "Flores, Hector",
+        "Bloque": "COALICION CIVICA",
+        "Correo": "hflores@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mfrade.jpg",
+        "Diputado": "Frade, Monica Edith",
+        "Bloque": "COALICION CIVICA",
+        "Correo": "mfrade@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mstilman.jpg",
+        "Diputado": "Stilman, Mariana",
+        "Bloque": "COALICION CIVICA",
+        "Correo": "mstilman@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/gcamano.jpg",
+        "Diputado": "Camaño, Graciela",
+        "Bloque": "CONSENSO FEDERAL",
+        "Correo": "gcamano@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/aerodriguez.jpg",
+        "Diputado": "Rodriguez, Alejandro \"Topo\"",
+        "Bloque": "CONSENSO FEDERAL",
+        "Correo": "aerodriguez@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/jsarghini.jpg",
+        "Diputado": "Sarghini, Jorge Emilio",
+        "Bloque": "CONSENSO FEDERAL",
+        "Correo": "jsarghini@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/rdelpla.jpg",
+        "Diputado": "Del Pla, Romina",
+        "Bloque": "FRENTE DE IZQUIERDA Y DE LOS TRABAJADORES",
+        "Correo": "rdelpla@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/jalderete.jpg",
+        "Diputado": "Alderete, Juan Carlos",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "jalderete@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/malvarezr.jpg",
+        "Diputado": "Alvarez Rodriguez, Maria Cristina",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "malvarezr@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/aaparicio.jpg",
+        "Diputado": "Aparicio, Alicia N.",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "aaparicio@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/cbernazza.jpg",
+        "Diputado": "Bernazza, Claudia Alicia",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "cbernazza@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/lbormioli.jpg",
+        "Diputado": "Bormioli, Lisandro",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "lbormioli@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/wcorrea.jpg",
+        "Diputado": "Correa, Walter",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "wcorrea@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/jdemendiguren.jpg",
+        "Diputado": "De Mendiguren, Jose Ignacio",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "jdemendiguren@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/ffagioli.jpg",
+        "Diputado": "Fagioli, Federico",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "ffagioli@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/hfernandez.jpg",
+        "Diputado": "Fernandez, Hector",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "hfernandez@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/lgrosso.jpg",
+        "Diputado": "Grosso, Leonardo",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "lgrosso@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/crgutierrez.jpg",
+        "Diputado": "Gutierrez, Ramiro",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "crgutierrez@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mkirchner.jpg",
+        "Diputado": "Kirchner, Maximo Carlos",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "mkirchner@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mkoenig.jpg",
+        "Diputado": "Koenig, Marcelo",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "mkoenig@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/flampreabe.jpg",
+        "Diputado": "Lampreabe, Florencia",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "flampreabe@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mjlopez.jpg",
+        "Diputado": "Lopez, Jimena",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "mjlopez@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mmacha.jpg",
+        "Diputado": "Macha, Monica",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "mmacha@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mrmartinez.jpg",
+        "Diputado": "Martinez, Maria Rosa",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "mrmartinez@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/smassa.jpg",
+        "Diputado": "Massa, Sergio Tomas",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "smassa@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/cmoreau.jpg",
+        "Diputado": "Moreau, Cecilia",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "cmoreau@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/lmoreau.jpg",
+        "Diputado": "Moreau, Leopoldo Raul Guido",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "lmoreau@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/fmoyano.jpg",
+        "Diputado": "Moyano, Juan Facundo",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "fmoyano@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/cormachea.jpg",
+        "Diputado": "Ormachea, Claudia Beatriz",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "cormachea@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/nrodriguezsaa.jpg",
+        "Diputado": "Rodriguez Saa, Nicolas",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "nrodriguezsaa@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/lrusso.jpg",
+        "Diputado": "Russo, Laura",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "lrusso@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mschwindt.jpg",
+        "Diputado": "Schwindt, Maria Liliana",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "mschwindt@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/cselva.jpg",
+        "Diputado": "Selva, Carlos Americo",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "cselva@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/asierra.jpg",
+        "Diputado": "Sierra, Magdalena",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "asierra@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/vsiley.jpg",
+        "Diputado": "Siley, Vanesa",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "vsiley@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/ltailhade.jpg",
+        "Diputado": "Tailhade, Luis Rodolfo",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "ltailhade@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mtundis.jpg",
+        "Diputado": "Tundis, Mirta",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "mtundis@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/ruhrig.jpg",
+        "Diputado": "Uhrig, Romina",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "ruhrig@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mvallejos.jpg",
+        "Diputado": "Vallejos, Fernanda",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "mvallejos@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/dvilar.jpg",
+        "Diputado": "Vilar, Daniela Marina",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "dvilar@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/lyambrun.jpg",
+        "Diputado": "Yambrun, Liliana Patricia",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "lyambrun@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/hyasky.jpg",
+        "Diputado": "Yasky, Hugo",
+        "Bloque": "FRENTE DE TODOS",
+        "Correo": "hyasky@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/ebucca.jpg",
+        "Diputado": "Bucca, Eduardo",
+        "Bloque": "JUSTICIALISTA",
+        "Correo": "ebucca@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/jaicega.jpg",
+        "Diputado": "Aicega, Juan",
+        "Bloque": "PRO",
+        "Correo": "jaicega@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/aasseff.jpg",
+        "Diputado": "Asseff, Alberto Emilio",
+        "Bloque": "PRO",
+        "Correo": "aasseff@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/hberisso.jpg",
+        "Diputado": "Berisso, Hernan",
+        "Bloque": "PRO",
+        "Correo": "hberisso@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/acaceres.jpg",
+        "Diputado": "Caceres, Adriana",
+        "Bloque": "PRO",
+        "Correo": "acaceres@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/ccrescimbeni.jpg",
+        "Diputado": "Crescimbeni, Camila",
+        "Bloque": "PRO",
+        "Correo": "ccrescimbeni@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/eflangan.jpg",
+        "Diputado": "Fernandez Langan, Ezequiel",
+        "Bloque": "PRO",
+        "Correo": "eflangan@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/rgarciadeluca.jpg",
+        "Diputado": "Garcia De Luca, Sebastian",
+        "Bloque": "PRO",
+        "Correo": "rgarciadeluca@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mjoury.jpg",
+        "Diputado": "Joury, Maria De Las Mercedes",
+        "Bloque": "PRO",
+        "Correo": "mjoury@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/slospennato.jpg",
+        "Diputado": "Lospennato, Silvia Gabriela",
+        "Bloque": "PRO",
+        "Correo": "slospennato@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mmedina.jpg",
+        "Diputado": "Medina, Martin Nicolas",
+        "Bloque": "PRO",
+        "Correo": "mmedina@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mocana.jpg",
+        "Diputado": "Ocaña, Maria Graciela",
+        "Bloque": "PRO",
+        "Correo": "mocana@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mpiccolomini.jpg",
+        "Diputado": "Piccolomini, Maria Carla",
+        "Bloque": "PRO",
+        "Correo": "mpiccolomini@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mlrey.jpg",
+        "Diputado": "Rey, Maria Lujan",
+        "Bloque": "PRO",
+        "Correo": "mlrey@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/critondo.jpg",
+        "Diputado": "Ritondo, Cristian Adrian",
+        "Bloque": "PRO",
+        "Correo": "critondo@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/ptorello.jpg",
+        "Diputado": "Torello, Pablo",
+        "Bloque": "PRO",
+        "Correo": "ptorello@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/nvilla.jpg",
+        "Diputado": "Villa, Natalia Soledad",
+        "Bloque": "PRO",
+        "Correo": "nvilla@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/wwolff.jpg",
+        "Diputado": "Wolff, Waldo Ezequiel",
+        "Bloque": "PRO",
+        "Correo": "wwolff@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/ndelcano.jpg",
+        "Diputado": "Del Caño, Nicolas",
+        "Bloque": "PTS - FRENTE DE IZQUIERDA",
+        "Correo": "ndelcano@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/kbanfi.jpg",
+        "Diputado": "Banfi, Karina",
+        "Bloque": "UCR",
+        "Correo": "kbanfi@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/mbazze.jpg",
+        "Diputado": "Bazze, Miguel Angel",
+        "Bloque": "UCR",
+        "Correo": "mbazze@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/cfernandez.jpg",
+        "Diputado": "Fernandez, Carlos Alberto",
+        "Bloque": "UCR",
+        "Correo": "cfernandez@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/jomendoza.jpg",
+        "Diputado": "Mendoza, Josefina",
+        "Bloque": "UCR",
+        "Correo": "jomendoza@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/fquetglas.jpg",
+        "Diputado": "Quetglas, Fabio Jose",
+        "Bloque": "UCR",
+        "Correo": "fquetglas@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/ssalvador.jpg",
+        "Diputado": "Salvador, Sebastian Nicolas",
+        "Bloque": "UCR",
+        "Correo": "ssalvador@hcdn.gob.ar"
+    },
+    {
+        "Foto": "https://www4.hcdn.gob.ar/fotos/cuadradas/pansaloni.jpg",
+        "Diputado": "Ansaloni, Pablo Miguel",
+        "Bloque": "UNIDAD Y EQUIDAD FEDERAL",
+        "Correo": "pansaloni@hcdn.gob.ar"
+    }
+];
+var htmlTemplate = "<div class='col-md-4 col-12'>" +
+                        "<div class='box-part shadow-sm text-center'>" +
+                            "<img src='#FOTO#' style='width: 100px; height: 100px'/>" +
+                            "<h4 style='font-size: 1rem;'>#NOMBRE#</h4>" +
+                            "<h4 style='color: lightslategray; font-size: 1rem;'>#BLOQUE#</h4>" +
+                            "<a target='_blank' href='#LINK#' alt='Enviar mail'>" +
+                                "<i class='fa fa-envelope' aria-hidden='true' style='font-size: 2rem;color: var(--primary-color);'></i>" +
+                            "</a>" +
+                        "</div>" +
+                    "</div>";
+var mailSubject = "No a la Reforma Judicial";
+var mailBody= 'Señor Legislador/ra por medio de la presente le solicito que en el día en que se trate la "Reforma Judicial" conocida con el nombre de "LEY DE ORGANIZACIÓN Y COMPETENCIA DE LA JUSTICIA FEDERAL CON ASIENTO ' +
+              'EN LA CIUDAD AUTÓNOMA DE BUENOS AIRES Y EN LAS PROVINCIAS", se abstenga de hacerse presente en la Cámara para evitar el quórum, y de esta forma no ser cómplice de un gobierno que tiene las prioridades alteradas. ' +
+              'Cómo parte del pueblo, le informo que hay otros temas que son prioridades. Revistiendo el carácter de NECESARIAS y URGENTES, para tratar y sesionar en este contexto de emergencia sanitaria, económica y social. ' +
+              'Desde ya apelo a qué realizará lo correcto no dando quórum y evitando ser cómplice de un gobierno oportunista. Saludos cordiales!';
+
+function getlinkMailto(i){
+    return 'mailto:' + jsonDiputados[i].Correo +'?subject='+ encodeURIComponent(mailSubject) + '&body=' + encodeURIComponent(mailBody);
+}
+function getHtmlWithIndex(i) {
+    var temp = htmlTemplate;
+    temp = temp.replace("#FOTO#",jsonDiputados[i].Foto);
+    temp = temp.replace("#BLOQUE#",jsonDiputados[i].Bloque);
+    temp = temp.replace("#NOMBRE#",jsonDiputados[i].Diputado);
+    temp = temp.replace("#LINK#",getlinkMailto(i));
+
+    return temp;
+}
+
